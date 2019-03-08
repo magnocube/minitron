8e48351361636fbb2643d881d6ba16701d0cf42b
Magnocube <stefangroenendijk1998@hotmail.com>
2019-03-08 12:20:32 +0100

    completed basic motor control

--- esp32_firmware/main/main.cpp
+++ esp32_firmware/main/main.cpp
@@ -10,31 +10,45 @@ void core0Task( void * pvParameters ){
      uint32_t loopCounter=0;
     while(true)
     {
+        
         //mpu9250ReadMotion();//takes 0.65ms
         //mpu9250ReadCompass();//takes 0.5ms
 
         //irDecoder.read();//takes 0.005ms
-        if(loopCounter%100 == 0)
-        {
-            //irDecoder.send();//takes 0.03ms
+        // if(loopCounter%100 == 0)
+        // {
+        //     //irDecoder.send();//takes 0.03ms
 
-        }
+        // }
         //irDecoder.runProximity();//takes 90ms
 
         loopCounter++;
         printf("test\n");
-        vTaskDelay(1000/portTICK_PERIOD_MS);
+        
+        MotorController->setTargetSpeed(-150,-150);
+        vTaskDelay(50/portTICK_PERIOD_MS);
+        // MotorController->setTargetSpeed(-1000,-1000);
+        // vTaskDelay(50/portTICK_PERIOD_MS);
     }
 }
 void core1Task( void * pvParameters ){
 
-
+    MotorController->setTargetSpeed(10000,10000);
     while(true)
     {
+
+        // for(int i = 0 ; i < 1000; i+= 10) {
+        //     MotorController->setSpeed(i,i);
+        //     vTaskDelay(10/portTICK_PERIOD_MS);
+        // }
+   
         vTaskDelay(10/portTICK_PERIOD_MS);
+        MotorController->loop();
         if(Camera->dataAnvailable()){
             Camera->ReadData(); // will also sand a confirmation
         }
+        //MotorController.loop();
+       
         // MVCamera.setCameraAngle(10);
         // vTaskDelay(800/portTICK_PERIOD_MS);
         // MVCamera.setCameraAngle(170);
@@ -57,13 +71,13 @@ extern "C" void app_main()
                     1,          /* Priority of the task */
                     NULL,       /* Task handle. */
                     0); //core 0
-    /*xTaskCreatePinnedToCore(core1Task, "core1Task", 
+    xTaskCreatePinnedToCore(core1Task, "core1Task", 
                     10000,      // Stack size in words 
                     NULL,       // Task input parameter 
                     1,          // Priority of the task 
                     NULL,       // Task handle. 
                     1); //core 1
-*/
+
     while(true)
     {
         vTaskDelay(portMAX_DELAY);
