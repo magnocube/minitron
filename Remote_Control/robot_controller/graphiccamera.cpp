f4a837f7befc279a488ce3318c394f1e413e497a
Rene Schouten <rene@twtg.io>
2019-03-26 23:07:09 +0100

    added visual camera ball location

---
+++ Remote_Control/robot_controller/graphiccamera.cpp
@@ -0,0 +1,6 @@
+#include "graphiccamera.h"
+
+graphicCamera::graphicCamera()
+{
+
+}
---
+++ Remote_Control/robot_controller/graphiccamera.h
@@ -0,0 +1,11 @@
+#ifndef GRAPHICCAMERA_H
+#define GRAPHICCAMERA_H
+
+
+class graphicCamera
+{
+public:
+    graphicCamera();
+};
+
+#endif // GRAPHICCAMERA_H
